5007761f53e5b8948ff265a296afd29b248f99bc
medeiros13 <gabrielmedeiros13122000@gmail.com>
2022-02-04 11:40:47 -0300

    Aula 2 finalizada, nessa aula aprendemos algumas operações com arrays, como clonar, copiar, limpar elementos, ordenar, inverter a ordem, preencher a última posição e trocar o tamanho do array.

--- CSharp_Collections/Program.cs
+++ CSharp_Collections/Program.cs
@@ -54,6 +54,73 @@
             Console.WriteLine("Tentando alterar o valor do índice 0 do array aulasImplicito:");
             aulasImplicito[0] = "Trabalhando com Arrays";
             ImprimirIndices(aulasImplicito);
+
+            //Para descobrirmos em qual índice está o nosso valor, utilizamos a método estático IndexOf da classe Array, passando por parâmetro em qual array devemos buscar o valor, e o valor a ser buscado
+            Console.WriteLine();
+            Console.WriteLine("Usando o IndexOf para buscar o índice da aulaModelando:");
+            Console.WriteLine($"A aula modelando está no índice {Array.IndexOf(aulasImplicito, aulaModelando)}");
+
+            //Se desejamos achar a última ocorrência do nosso valor no array, utilizamos o método LastIndexOf da classe Array, passando por parâmetro em qual array devemos buscar o valor, e o valor a ser buscado
+            Console.WriteLine();
+            Console.WriteLine("Usando o LastIndexOf para buscar a última ocorrência de aulaModelando no nosso array:");
+            Console.WriteLine($"A aula modelando está no índice {Array.LastIndexOf(aulasImplicito, aulaModelando)}");
+
+            //Se desejamos trocar a ordem do nosso Array, onde o último seria o primeiro, e assim sucessivamente, utilizamos o método Reverso da classe Array, passando por parâmetro qual array desejamos trocar a ordem
+            Console.WriteLine();
+            Console.WriteLine("Usando o Reverse para trocarmos a ordem dos índices do array:");
+            Array.Reverse(aulasImplicito);
+            ImprimirIndices(aulasImplicito);
+            Console.WriteLine();
+            Console.WriteLine("Usando o Reverse para voltar o array para a ordem natural:");
+            Array.Reverse(aulasImplicito);
+            ImprimirIndices(aulasImplicito);
+
+            //Se desejamos redimensionar o tamanho do nosso array, por exemplo, se ele é de tamanho 3 e eu quero transformar em tamanho 2,
+            //Utilizamos o método Resize da classe Array, passando por parâmetro o array que desejamos redimensionar (passamos esse array por referência), e o novo tamanho para esse array
+            Console.WriteLine();
+            Console.WriteLine("Usando o Resize reduzir o tamanho do array para 2 índices:");
+            Array.Resize(ref aulasImplicito, 2);
+            ImprimirIndices(aulasImplicito);
+
+            //Se desejamos aumentar o tamanho do array, para 3 índices novamente, vamos usar o Resize de novo
+            //Observe que a última posição foi preenchida com null, pois não passamos um valor para ela
+            Console.WriteLine();
+            Console.WriteLine("Usando o Resize voltar o array para o tamanho original:");
+            Array.Resize(ref aulasImplicito, 3);
+            ImprimirIndices(aulasImplicito);
+
+            //Portanto, para preenchermos a última posição de um array na qual não sabemos o tamanho exato, utilizamos o índice [<nomeDoArray>.Length -1]
+            //Pois ele irá sempre pegar o tamanho do array -1 que é o equivalente a última posição do array
+            Console.WriteLine();
+            Console.WriteLine("Usando o [aulasImplicito.Length -1] para atribuir o valor 'Conclusão' na última posição do array:");
+            aulasImplicito[aulasImplicito.Length - 1] = "Conclusão";
+            ImprimirIndices(aulasImplicito);
+
+            //Se desejamos ordenar o nosso array por ordem alfabética, devemos utilizar o método estático Sort da classe Array
+            Console.WriteLine();
+            Console.WriteLine("Usando o método Sort para ordenar os nosso valores em ordem alfabética:");
+            Array.Sort(aulasImplicito);
+            ImprimirIndices(aulasImplicito);
+
+            //Se desejamos copiar um array, utilizamos o método estático Copy da classe Array
+            //passando por parâmetro o array original que desejamos copiar, a posição do array original que iniciaremos a cópia,
+            //o array cópia, a posição inicial do array cópia, e o tamanho de índices que vão ser copiados
+            Console.WriteLine();
+            Console.WriteLine("Utilizando o método Copy para copiar as 2 últimas posições do array:");
+            string[] copia = new string[2];
+            Array.Copy(aulasImplicito, 1, copia, 0, 2);
+            ImprimirIndices(copia);
+
+            //Se desejamos clonar um array, utilizaremos o método Clone do nosso array, convertendo ele para um array de strings:
+            Console.WriteLine();
+            Console.WriteLine("Utilizando o método Clone para clonar o nosso array:");
+            string[] clone = aulasImplicito.Clone() as string[];
+            ImprimirIndices(clone);
+
+            //Se desejamos limpar os 2 últimos elementos do nosso array, utilizamos o método estático Clear da classe Array, passando o nome do array,
+            //a posição inicial da limpeza, e a quantidade de posições que desejamos limpar
+            Array.Clear(clone, 1, 2);
+            ImprimirIndices(clone);
         }
 
         private static void ImprimirIndices(string[] aulas)
