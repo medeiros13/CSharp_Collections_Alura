eae999180706ab8873d02f3daa322e84e45dd24f
medeiros13 <gabrielmedeiros13122000@gmail.com>
2022-02-12 17:31:49 -0300

    Aula 14 finalizada. Nessa aula aprendemos o funcionamento da collection Stack ou Pilha, e simulamos a ideia dos botões de próxima página e página anterior dos navegadores da internet. A Stack implementa a ideia de que o último que entra é o primeiro a sair ou LIFO(Last In First Out).

---
+++ CSharp_Collections/Models/Navegador.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharp_Collections.Models
+{
+    internal class Navegador
+    {
+        //Inicializamos uma Pilha (Stack) da seguinte forma:
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+        private string atual = "vazia";
+        public Navegador()
+        {
+            Console.WriteLine($"Página atual: {atual}");
+        }
+        internal void NavegarPara(string url)
+        {
+            //Para adicionarmos em uma Stack, utilizamos o método Push();
+            historicoAnterior.Push(atual);
+            atual = url;
+            Console.WriteLine($"Página atual: {atual}");
+        }
+        internal void Anterior()
+        {
+            //Para voltarmos para o elemento anterior na Stack, utilizamos o método Pop();
+            //Porém, o método Pop() remove da pilha o elemento atual e vai para o anterior,
+            //caso não tenha mair elemento anterior, o Pop irá lançar uma exceção
+            //Portanto, vamo fazer uma verificação se existe elementos na Stack ainda, para isso,
+            //Iremos utilizar o método Any():
+            if (historicoAnterior.Any())
+            {
+                historicoProximo.Push(atual);
+                atual = historicoAnterior.Pop();
+                Console.WriteLine($"Página atual: {atual}");
+            }
+        }
+        internal void Proximo()
+        {
+            if (historicoProximo.Any())
+            {
+                historicoAnterior.Push(atual);
+                atual = historicoProximo.Pop();
+                Console.WriteLine($"Página atual: {atual}");
+            }
+        }
+    }
+}
